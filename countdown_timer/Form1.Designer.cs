0af9835921fd90b2987ff7063978267034c4d9b8
siilverred <charlenesuhardjo@gmail.com>
2025-12-30 14:37:05 +0700

    Add project files.

---
+++ countdown_timer/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace countdown_timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.numHour = new System.Windows.Forms.NumericUpDown();
+            this.numMinute = new System.Windows.Forms.NumericUpDown();
+            this.numSecond = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSecond)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // numHour
+            // 
+            this.numHour.Location = new System.Drawing.Point(22, 36);
+            this.numHour.Name = "numHour";
+            this.numHour.Size = new System.Drawing.Size(69, 20);
+            this.numHour.TabIndex = 1;
+            // 
+            // numMinute
+            // 
+            this.numMinute.Location = new System.Drawing.Point(97, 36);
+            this.numMinute.Name = "numMinute";
+            this.numMinute.Size = new System.Drawing.Size(69, 20);
+            this.numMinute.TabIndex = 2;
+            // 
+            // numSecond
+            // 
+            this.numSecond.Location = new System.Drawing.Point(172, 36);
+            this.numSecond.Name = "numSecond";
+            this.numSecond.Size = new System.Drawing.Size(69, 20);
+            this.numSecond.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 198);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numSecond);
+            this.Controls.Add(this.numMinute);
+            this.Controls.Add(this.numHour);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSecond)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numHour;
+        private System.Windows.Forms.NumericUpDown numMinute;
+        private System.Windows.Forms.NumericUpDown numSecond;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ countdown_timer/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace countdown_timer
+{
+    public partial class Form1 : Form
+    {
+        int remainingTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "00:00:00";
+
+            numHour.ValueChanged += UpdateTimeFromNumeric;
+            numMinute.ValueChanged += UpdateTimeFromNumeric;
+            numSecond.ValueChanged += UpdateTimeFromNumeric;
+
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+        }
+
+
+        private void UpdateTimeFromNumeric(object sender, EventArgs e)
+        {
+            remainingTime = (int)numHour.Value * 3600 + (int)numMinute.Value * 60 + (int)numSecond.Value;
+            UpdateLabel();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (remainingTime > 0)
+            {
+                remainingTime--;
+                UpdateLabel();
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Time's up!");
+            }
+        }
+
+        private void UpdateLabel()
+        {
+
+            TimeSpan time = TimeSpan.FromSeconds(remainingTime);
+            label1.Text = time.ToString(@"hh\:mm\:ss");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnStart_Click(null, null);
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                btnStop_Click(null, null);
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Delete)
+            {
+                btnReset_Click(null, null);
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            remainingTime = 0;
+            numHour.Value = 0;
+            numMinute.Value = 0;
+            numSecond.Value = 0;
+            UpdateLabel();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+    }
+}
